fb54f0d30f5ce6b45fb8d0bc59069cc4130d3f31
Hariom Singh Thakur <hariomthakur638@gmail.com>
2025-12-12 19:24:24 +0530

    remote repo added to master branch

---
+++ oops/plymrphism.java
@@ -0,0 +1,39 @@
+public class plymrphism {
+
+    // polymorphism in java is of two types - 1)
+
+    // public static class calculate {
+
+    // float length, height, radius, edge;
+
+    // // method overloading
+
+    // public float area(float length, float height) {
+    // this.length = length;
+    // this.height = height;
+    // return length * height; // reactangle area
+    // }
+
+    // public float area(float radius) {
+
+    // this.radius = radius;
+    // return 3.14f * radius * radius; // circle area
+    // }
+
+    // public float area(float edge, int n) {
+
+    // this.edge = edge;
+    // return 6 * edge * edge; // cube area
+
+    // }
+    // }
+
+    public static void main(String[] args) {
+
+        // calculate obj = new calculate();
+        // System.out.println("Area of rectangle = " + obj.area(5.0f, 4.0f));
+        // System.out.println("Area of circle = " + obj.area(7.0f));
+        // System.out.println("area of cube = " + obj.area(3.4f,1));
+
+    }
+}
