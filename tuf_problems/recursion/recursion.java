34e8873cac3aa7177c57b6303101f21347bd6de1
Hariom Singh Thakur <126336846+hariom-hub@users.noreply.github.com>
2025-12-15 12:24:36 +0000

    recursion directory added

--- tuf_problems/recursion/recursion.java
+++ tuf_problems/recursion/recursion.java
@@ -2,10 +2,44 @@ package recursion;
 
 import java.util.*;
 
-public class recursion{
+public class recursion {
 
-    public static void main(String[] args){
+    // static int count = 0;
+    // public static void printName() {
 
-        System.out.println("hello hariom.");
+    // System.out.println("Hariom");
+    // count++;
+    // if(count == 5){
+    // return;
+    // }
+    // printName();
+    // }
+
+    public static void NtoOne(int n) {
+
+        System.out.println(n);
+        if (n == 1) {
+            return;
+        }
+        NtoOne(n - 1);
+    }
+
+    public static void OnetoN(int n) {
+
+        if (n == 0) {
+            return;
+        }
+
+        OnetoN(n - 1);
+        System.out.println(n);
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer : ");
+        int n = sc.nextInt();
+        OnetoN(n);
     }
 }
